0e371e12fac4b542052ef5700dc4e87a59c9bb46
Jaum1204 <joaopedromarujo@outlook.com>
2025-01-30 16:36:03 -0300

    03 javascript finished

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    list = word.split('');
+    list.reverse();
+    word = list.join('');
+    return word;
 };
 
 // Do not edit below this line
