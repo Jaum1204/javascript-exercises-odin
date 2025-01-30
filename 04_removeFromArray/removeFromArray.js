9ecd0d83a8b31ffacba23488f52902814b1fc9d1
Jaum1204 <joaopedromarujo@outlook.com>
2025-01-30 17:23:20 -0300

    04 javascript finished

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    new_arr = []
+    arr.forEach(function(x){
+        if(!args.includes(x)){
+            new_arr.push(x);
+        }
+    })
+    return new_arr;
 };
 
 // Do not edit below this line
