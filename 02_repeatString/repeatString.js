176059218b77c318a07944221947c6ea61ba2258
Jaum1204 <joaopedromarujo@outlook.com>
2025-01-30 16:30:36 -0300

    02 javascript finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    let tot = '';
+    if (num < 0){
+        return "ERROR";
+    }
+    for(let i = 0; i < num; i++){
+        tot += str;
+    }
+    return tot;
 };
 
 // Do not edit below this line
